8a4bc723d154dcb250dc9a2750d2d01025438b6a
Kara O <karajo.odle@gmail.com>
2019-03-24 17:21:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,31 @@
 def valid_move?(board, index)
   !(position_taken?(board, index)) && index.between?(0, 8)
+
+  if !a && b
+    return true
+  return false
+
+  if !a
+    if b
+      return true
+  return false
 end
 
 
 def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
+
+  # dumb example of using if statements for this
+  if board[index] == "X"
+    return true
+  elsif board[index] == "O"
+    return true
+  else
+    return false
+
+  # another example
+  if board[index] == "X" || board[index] == "O"
+    return true
+  return false
+
 end
